916d4da45928a3decb1178c29b3664a6d096e595
ruttoa <janne.rattya@gmail.com>
2016-03-26 17:24:40 +0200

    Add entry-content class for page content

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,6 @@
-<?php the_content(); ?>
+<div class="entry-content">
+  <?php the_content(); ?>
+</div>
+<footer>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'janne'), 'after' => '</p></nav>']); ?>
+</footer>
